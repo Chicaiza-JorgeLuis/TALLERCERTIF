f3e1aff16a2988bdbdc14b2436e2765d4995e9a4
chicaizajaquejorge@outlook.com <chicaizajaquejorge@outlook.com>
2020-06-11 13:23:43 -0500

    Taller

---
+++ SlnTaller/BEUTaller/Transactions/PeliculaBLL.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUTaller.Transactions
+{
+    public class PeliculaBLL
+    {
+        public static void Create(Pelicula p)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Pelicula.Add(p);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+
+        public static Pelicula Get(int? id)
+        {
+            Entities db = new Entities();
+            return db.Pelicula.Find(id);
+        }
+
+        public static void Update(Pelicula pelicula)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Pelicula.Attach(pelicula);
+                        db.Entry(pelicula).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Pelicula pelicula = db.Pelicula.Find(id);
+                        db.Entry(pelicula).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+
+        public static List<Pelicula> List()
+        {
+            Entities db = new Entities();
+            //Instancia del contexto
+
+            /*List<Alumno> alumons = db.Alumnoes.ToList();
+            List<Alumno> resultado = new List<Alumno>();
+            foreach (Alumno a in alumons) {
+                if (a.sexo == "M") {
+                    resultado.Add(a);
+                }
+            }
+            return resultado;*/
+            //SQL -> SELECT * FROM dbo.Alumno WHERE sexo = 'M'
+            //return db.Alumnoes.Where(x => x.sexo == "M").ToList();
+
+            return db.Pelicula.ToList();
+
+            //Los metodos del EntityFramework se denomina Linq, 
+            //y la evluacion de condiciones lambda
+        }
+
+
+    
+
+  
+    }
+}
---
+++ SlnTaller/Taller/Controllers/PeliculasController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BEUTaller;
+using BEUTaller.Transactions;
+namespace Taller.Controllers
+{
+    public class PeliculasController : Controller
+    {
+       /// private Entities db = new Entities();
+
+        // GET: Peliculas
+        public ActionResult Index()
+        {
+            return View(PeliculaBLL.List());
+          
+        }
+
+        // GET: Peliculas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Peliculas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,fecha_lanzamiento,categoria,duracion")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                PeliculaBLL.Create(pelicula);
+                return RedirectToAction("Index");
+            }
+
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,fecha_lanzamiento,categoria,duracion")] Pelicula pelicula)
+        {
+            if (ModelState.IsValid)
+            {
+                PeliculaBLL.Update(pelicula);
+                return RedirectToAction("Index");
+            }
+            return View(pelicula);
+        }
+
+        // GET: Peliculas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pelicula pelicula = PeliculaBLL.Get(id);
+            if (pelicula == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pelicula);
+        }
+
+        // POST: Peliculas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PeliculaBLL.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+     
+    }
+}
